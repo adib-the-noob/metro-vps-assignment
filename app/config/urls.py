116b28a52f96b170ee4c589dee8270b2a3447454
adib-the-noob <mohammed.adib1104@gmail.com>
2025-07-29 20:07:28 +0600

    serializers added

--- app/config/urls.py
+++ app/config/urls.py
@@ -16,10 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health', health_check),
+
     # apps
-    path('v1/auth/', include('authcore.urls')),
-    path('v1/subscriptions/', include('subscriptions.urls')),
+    path('api/auth/', include('authcore.urls')),
+    path('api/subscriptions/', include('subscriptions.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
