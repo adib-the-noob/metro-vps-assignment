33559eb619541ebf5f6d01c1fde82335c4518ff0
adib-the-noob <mohammed.adib1104@gmail.com>
2025-08-01 16:16:40 +0600

    url changed

--- app/utils/caller.py
+++ app/utils/caller.py
@@ -6,13 +6,12 @@ load_dotenv()
 
 EXCHANGE_RATE_API_KEY = os.getenv("EXCHANGE_RATE_API_KEY")
 
-def compare_exchange_rate(base_currency=None, target_currency=None, amount: float = 1.0):
+def compare_exchange_rate(base_currency=None, target_currency=None):
     """
         base_currency: The currency to convert from (e.g., "USD").
         target_currency: The currency to convert to (e.g., "EUR").
-        amount: The amount to convert (not used in this function, but can be used for future enhancements).
     """
-    url = f"https://v6.exchangerate-api.com/v6/{EXCHANGE_RATE_API_KEY}/pair/{base_currency}/{target_currency}/{amount}"
+    url = f"https://v6.exchangerate-api.com/v6/{EXCHANGE_RATE_API_KEY}/pair/{base_currency}/{target_currency}"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
